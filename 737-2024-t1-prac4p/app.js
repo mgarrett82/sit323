f684aabd170aee49991bce7c083c539d5aa112bc
Michael Garrett <57734446+mgarrett82@users.noreply.github.com>
2024-04-02 13:27:58 +1100

    final update

--- 737-2024-t1-prac4p/app.js
+++ 737-2024-t1-prac4p/app.js
@@ -7,7 +7,7 @@ const app = express();
 //creates 2 routes to the modules with the calculation functions
 
 const router = require('./routes/basicCalculations.js');
-const routerAdvanced = require('./routes/advanacedCalculations.js');
+const routerAdvanced = require('./routes/advancedCalculations.js');
 
 
 //tells express to use the two routes
